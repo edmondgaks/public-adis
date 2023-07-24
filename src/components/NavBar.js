095baf83dd0670670e7c1706b787f114f60b4874
edmondgaks <edmondgaks@gmail.com>
2023-07-24 10:21:58 +0200

    refactoring the navbar and other parts in the site

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,162 +1,139 @@
+import React, { useState } from "react";
 import TopBar from "./TopBar";
 
+const DropdownMenuItem = ({ title, subMenuItems }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <li className="nav-item dropdown">
+      <a
+        href="#"
+        onClick={toggleMenu}
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+        className="nav-link dropdown-toggle"
+      >
+        {title} <i className="fa fa-angle-down"></i>
+      </a>
+      {isOpen && (
+        <ul className="dropdown-menu border-0 shadow" onClick={toggleMenu}>
+          {subMenuItems.map((item) => (
+            <li key={item.label}>
+              <a href={item.url} className="dropdown-item">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+};
+
 const Navbar = () => {
-    return (
-        <div className="background-header">
-            <TopBar />
-            <div className="container">
-                <div className="navigation">
-                <nav
-                    className="navbar navbar-expand-lg justify-content-between nav-color zeropadd">
-                    <div className="navbar-header">
-                    <a className="navbar-brand zeropadd" href="/">
-                        <img
-                        src="img/logo_200x200.png"
-                        alt="logo"
-                        className="max-width-60px"
-                        />
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                        <span className="navbar-toggler-icon"></span>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    </div>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="nav navbar-nav ml-auto">
-                        <li className="nav-item">
-                        <a className="nav-link" href="/"
-                            >Inicio
-                            <span className="sr-only">(current)</span>
-                        </a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/category">Búsqueda avanzada</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <a
-                            href="#"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                            className="nav-link dropdown-toggle"
-                            >Categorias <i className="fa fa-angle-down"></i></a>
-                        <ul className="dropdown-menu border-0 shadow">
-                            <li>
-                            <a href="/category" className="dropdown-item">Trabajos </a>
-                            </li>
-                            <li>
-                            <a href="/category" className="dropdown-item">Propiedades</a>
-                            </li>
-                            <li className="dropdown-submenu">
-                            <a
-                                href="/category"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                                className="dropdown-item dropdown-toggle"
-                                >celulares</a>
-                            <ul className="dropdown-menu border-0 shadow">
-                                <li>
-                                <a
-                                    tabindex="-1"
-                                    href="/category"
-                                    className="dropdown-item"
-                                    >Apple</a>
-                                </li>
-                                <li>
-                                <a href="/category" className="dropdown-item"
-                                    >Samsung</a>
-                                </li>
-                                <li>
-                                <a href="/category" className="dropdown-item">LG</a>
-                                </li>
-                            </ul>
-                            </li>
-                            <li className="dropdown-submenu">
-                            <a
-                                href="/category"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                                className="dropdown-item dropdown-toggle">Carros</a>
-                            <ul className="dropdown-menu border-0 shadow">
-                                <li>
-                                <a
-                                    tabindex="-1"
-                                    href="/category"
-                                    className="dropdown-item">Toyota</a
-                                >
-                                </li>
-                                <li>
-                                <a href="/category" className="dropdown-item"
-                                    >Suzuki</a>
-                                </li>
-                                <li>
-                                <a href="/category" className="dropdown-item"
-                                    >Honda</a>
-                                </li>
-                            </ul>
-                            </li>
-                            <li>
-                            <a href="/category" className="dropdown-item">Renta</a>
-                            </li>
-                            <li>
-                            <a href="/category" className="dropdown-item"
-                                >Servicios</a
-                            >
-                            </li>
-                        </ul>
-                        </li>
+  const [menuItems] = useState([
+    { label: "Inicio", url: "/" },
+    { label: "Búsqueda avanzada", url: "/category" },
+    {
+      label: "Categorias",
+      subMenuItems: [
+        { label: "Trabajos", url: "/category" },
+        { label: "Propiedades", url: "/category" },
+        {
+          label: "Celulares",
+          subMenuItems: [
+            { label: "Apple", url: "/category" },
+            { label: "Samsung", url: "/category" },
+            { label: "LG", url: "/category" },
+          ],
+        },
+        {
+          label: "Carros",
+          subMenuItems: [
+            { label: "Toyota", url: "/category" },
+            { label: "Suzuki", url: "/category" },
+            { label: "Honda", url: "/category" },
+          ],
+        },
+        { label: "Renta", url: "/category" },
+        { label: "Servicios", url: "/category" },
+      ],
+    },
+    {
+      label: "Páginas",
+      subMenuItems: [
+        { label: "Info", url: "/about-us" },
+        { label: "Blog", url: "/blog" },
+        { label: "Contáctanos", url: "/contactus" },
+        { label: "Faq", url: "/faq" },
+      ],
+    },
+    { label: "Contáctanos", url: "/contactus" },
+    { label: "Login / Registro", url: "/loginRegister" },
+    { label: "Anuncia Gratis", url: "/postad" },
+  ]);
 
-                        <li className="nav-item dropdown">
-                        <a
-                            href="#"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                            className="nav-link dropdown-toggle"
-                            >Páginas <i className="fa fa-angle-down"></i></a>
-                        <ul className="dropdown-menu border-0 shadow">
-                            <li>
-                            <a href="/about-us" className="dropdown-item">Info</a>
-                            </li>
-                            <li><a href="blog" className="dropdown-item">Blog</a></li>
-                            <li>
-                            <a href="/contactus" className="dropdown-item">Contáctanos</a>
-                            </li>
-                            <li><a href="/faq" className="dropdown-item">Faq</a></li>
-                        </ul>
-                        </li>
-
-                        <li className="nav-item">
-                        <a className="nav-link" href="/contactus">Contáctanos</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/loginRegister"
-                            ><i className="fa fa-user" aria-hidden="true"></i>Login /Registro</a>
-                        </li>
-                        <li className="nav-item bordering">
-                        <a className="nav-link" href="/postad">Anuncia Gratis</a>
-                        </li>
-                    </ul>
-                    </div>
-                </nav>
-                </div>
+  return (
+    <div className="background-header">
+      <TopBar />
+      <div className="container">
+        <div className="navigation">
+          <nav className="navbar navbar-expand-lg justify-content-between nav-color zeropadd">
+            <div className="navbar-header">
+              <a className="navbar-brand zeropadd" href="/">
+                <img
+                  src="img/logo_200x200.png"
+                  alt="logo"
+                  className="max-width-60px"
+                />
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
+              </button>
             </div>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="nav navbar-nav ml-auto">
+                {menuItems.map((item) => {
+                  if (item.subMenuItems) {
+                    return (
+                      <DropdownMenuItem
+                        key={item.label}
+                        title={item.label}
+                        subMenuItems={item.subMenuItems}
+                      />
+                    );
+                  } else {
+                    return (
+                      <li className="nav-item" key={item.label}>
+                        <a className="nav-link" href={item.url}>
+                          {item.label}
+                        </a>
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            </div>
+          </nav>
         </div>
-    )
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
