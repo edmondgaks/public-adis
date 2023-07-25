76730d3deaa0d388de34608121932186e6f8cc52
edmondgaks <edmondgaks@gmail.com>
2023-07-25 18:44:29 +0200

    final touches

--- src/components/HomeCategories.js
+++ src/components/HomeCategories.js
@@ -1,107 +1,120 @@
 import React, { useState } from "react"
 const HomeCategories = () => {
-  const [categories] = useState([
-    {
-      icon: "maida-laptop",
-      text: "Laptops",
-    },
-    {
-      icon: "maida-briefcase",
-      text: "Empleos",
-    },
-    {
-      icon: "maida-t-shirt",
-      text: "Ropa",
-    },
-    {
-      icon: "maida-car-1",
-      text: "Carros",
-    },
-    {
-      icon: "maida-calculator",
-      text: "Auditors",
-    },
-    {
-      icon: "maida-zoo",
-      text: "máscotas",
-    },
-    {
-      icon: "maida-town-hall",
-      text: "Finance",
-    },
-    {
-      icon: "maida-art-gallery",
-      text: "Art",
-    },
-    {
-      icon: "maida-fast-food",
-      text: "Comida"
-    },
-    {
-      icon: "maida-bar",
-      text: "Club",
-    },
-    {
-      icon: "maida-delivery-transport-2",
-      text: "Logistics",
-    },
-    {
-      icon: "maida-display",
-      text: "Electronicos",
-    },
-    {
-      icon: "maida-cup",
-      text: "Cafe"
-    },
-    {
-      icon: "maida-case-medic",
-      text: "Doctores",
-    },
-    {
-      icon: "fa-film",
-      text: "Entretenimiento",
-    },
-    {
-      icon: "maida-soccer",
-      text: "Juegos",
-    },
-    {
-      icon: "maida-phone",
-      text: "Celulares",
-    },
-    {
-      icon: "F",
-      text: "Support",
-    },
-    {
-      icon: "fa-book",
-      text: "Libros",
-    },
-    {
-      icon: "fa-child",
-      text: "Para niños",
-    },
-    
-  ]);
-    return (
-      <div className="category-container">
-      <div className="container">
-        <h2 className="text-center styleh2 karma">
-          <a className="greens" href="/category">Categorias</a>
-        </h2>
-        <div className="cat-icons">
-          {categories.map((category, index) => (
-            <a href="#" key={index}>
-              <div className={`mainicon icon ${category.icon}`}>
-                <span className="text-icon">{category.text}</span>
+  return (
+    <div class="category-container">
+        <div class="container">
+          <h2 class="text-center styleh2 karma">
+            <a class="greens" href="#">Categorias</a>
+          </h2>
+          <div class="cat-icons">
+            <a href="#">
+              <div class="mainicon icon maida-laptop">
+                <span class="text-icon">Laptops</span>
               </div>
             </a>
-          ))}
-          <div className="clearfix"></div>
+            <a href="#">
+              <div class="mainicon icon maida-briefcase">
+                <span class="text-icon">Empleos</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-t-shirt">
+                <span class="text-icon">Ropa</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-car-1">
+                <span class="text-icon">Carros</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-calculator">
+                <span class="text-icon">Auditors</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-zoo">
+                <span class="text-icon">máscotas</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-town-hall">
+                <span class="text-icon">Finance</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-art-gallery">
+                <span class="text-icon">Art</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-fast-food">
+                <span class="text-icon">Comida</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-bar">
+                <span class="text-icon">Club</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-delivery-transport-2">
+                <span class="text-icon">Logistics</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-display">
+                <span class="text-icon">Electronicos</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-cup">
+                <span class="text-icon">Cafe</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-case-medic">
+                <span class="text-icon">Doctores</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon">
+                <i class="fa fa-film" aria-hidden="true"></i>
+                <span class="text-icon">Entretenimiento</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-soccer">
+                <span class="text-icon">Juegos</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon maida-phone">
+                <span class="text-icon">Celulares</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon icon" data-icon="F">
+                <span class="text-icon">Support</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon">
+                <i class="fa fa-book" aria-hidden="true"></i>
+                <span class="text-icon">Libros</span>
+              </div>
+            </a>
+            <a href="#">
+              <div class="mainicon">
+                <i class="fa fa-child" aria-hidden="true"></i>
+                <span class="text-icon">Para niños</span>
+              </div>
+            </a>
+            <div class="clearfix"></div>
+          </div>
         </div>
       </div>
-    </div>
-    )
+  )
 }
 
 export default HomeCategories
